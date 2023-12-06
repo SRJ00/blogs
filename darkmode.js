33c31004a4c8150dd7ad6e7d292c383cf2285ac3
Saurav Jadhav <59251003+SRJ00@users.noreply.github.com>
2023-12-06 20:45:25 +0530

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -2,7 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const body = document.body;
 
+    // Check if the user previously set dark mode
+    const isDarkModeEnabled = localStorage.getItem('darkMode') === 'enabled';
+
+    // Set dark mode by default
+    body.classList.toggle('dark-mode', isDarkModeEnabled);
+    darkModeToggle.checked = isDarkModeEnabled;
+
+    // Toggle dark mode when the user changes the checkbox
     darkModeToggle.addEventListener('change', function() {
-        body.classList.toggle('dark-mode', darkModeToggle.checked);
+        const isDarkMode = darkModeToggle.checked;
+        body.classList.toggle('dark-mode', isDarkMode);
+
+        // Save user preference to local storage
+        localStorage.setItem('darkMode', isDarkMode ? 'enabled' : 'disabled');
     });
 });
