4f4ac603d55de1b58bb79b5b7cb1d1c3acf5700f
Saurav Jadhav <59251003+SRJ00@users.noreply.github.com>
2024-01-05 20:15:25 +0530

    Update comments.js

--- comments.js
+++ comments.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const script = document.createElement('script');
     script.src = 'https://utteranc.es/client.js';
-    script.setAttribute('repo', 'srj00/Articles');
+    script.setAttribute('repo', 'srj00/blogs');
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('theme', 'github-light');
     script.setAttribute('crossorigin', 'anonymous');
